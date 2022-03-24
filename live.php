dce420dd4328c7c208a90f6535544a4d692c7a35
Avinash Reddy <avinashreddy3108@gmail.com>
2022-03-24 17:50:56 +0530

    live.php: fix API URL

--- live.php
+++ live.php
@@ -20,7 +20,7 @@ if ($token != "" && @$channel_target != "")
 $opts = ['http' => ['method' => 'GET', 'header' => 'User-Agent: plaYtv/6.0.9 (Linux; Android 5.1.1) ExoPlayerLib/2.13.2']];
 $cx = stream_context_create($opts);
 
-$hs = file_get_contents("https://jiotvweb.cdn.jio.com/jiotv.live.cdn.jio.com/" . $channel_target . "/" . $channel_target . "_" . $_REQUEST["q"] . ".m3u8" . $token, false, $cx);
+$hs = file_get_contents("https://jiotv.live.cdn.jio.com/" . $channel_target . "/" . $channel_target . "_" . $_REQUEST["q"] . ".m3u8" . $token, false, $cx);
 $hs = @preg_replace("/" . $channel_target . "_" . $_REQUEST["q"] . "-([^.]+\.)key/", 'stream.php?key=' . $channel_target . '/' . $channel_target . '_' . $_REQUEST["q"] . '-\1key', $hs);
 $hs = @preg_replace("/" . $channel_target . "_" . $_REQUEST["q"] . "-([^.]+\.)ts/", 'stream.php?ts=' . $channel_target . '/' . $channel_target . '_' . $_REQUEST["q"] . '-\1ts', $hs);
 $hs = str_replace("https://tv.media.jio.com/streams_live/" . $channel_target . "/", "", $hs);
